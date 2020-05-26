b6900d37109faefc386ee291ef91d6867370c665
Ербол Сыздық <ysyzdyk@alfabank.kz>
2020-05-26 21:14:14 +0600

    first commit

---
+++ precache-manifest.37d6e04251bb895eaeb31b442be12559.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90277b8b5a7bba33cbfb",
+    "url": "/css/app.14bd5a21.css"
+  },
+  {
+    "revision": "a740de5f7c6d17276edf",
+    "url": "/css/chunk-vendors.7f89301a.css"
+  },
+  {
+    "revision": "80dd37823d80a0fc8746557c75083108",
+    "url": "/img/back.80dd3782.jpg"
+  },
+  {
+    "revision": "d270b8a1d1c6b3db8c9214fa5e955897",
+    "url": "/img/cards/1c.jpg"
+  },
+  {
+    "revision": "7f3a18243cedc85bcf9abe97c74edb58",
+    "url": "/img/cards/1d.jpg"
+  },
+  {
+    "revision": "dcc388a79b958def823686059c2fb2b4",
+    "url": "/img/cards/1h.jpg"
+  },
+  {
+    "revision": "60604ae4e9e98c0d68c36e869fe3311c",
+    "url": "/img/cards/1s.jpg"
+  },
+  {
+    "revision": "e3da674a439000983eb6e3c40fe15696",
+    "url": "/img/cards/6c.jpg"
+  },
+  {
+    "revision": "749d06b564a5791e87f5d54823f737c3",
+    "url": "/img/cards/6d.jpg"
+  },
+  {
+    "revision": "b4fe07151ae1aee9b26c3777275a2608",
+    "url": "/img/cards/6h.jpg"
+  },
+  {
+    "revision": "09da87237286dc36cdced4bac1df8994",
+    "url": "/img/cards/6s.jpg"
+  },
+  {
+    "revision": "4af61e3da3c4bf413d85c984c92556b2",
+    "url": "/img/cards/7c.jpg"
+  },
+  {
+    "revision": "ce20fc0f53ca7c6fa5398d089a45b6e3",
+    "url": "/img/cards/7d.jpg"
+  },
+  {
+    "revision": "779187957c40be2bb20245272d136cad",
+    "url": "/img/cards/7h.jpg"
+  },
+  {
+    "revision": "d74075c5101ad6f54dc2183551d7b875",
+    "url": "/img/cards/7s.jpg"
+  },
+  {
+    "revision": "39d47ceba50971c3f362dbf6a7315fb9",
+    "url": "/img/cards/8c.jpg"
+  },
+  {
+    "revision": "15a0035a82f980de7f49a2439059c7c2",
+    "url": "/img/cards/8d.jpg"
+  },
+  {
+    "revision": "d2fc52bdedb7de5b3530ba06ba08c504",
+    "url": "/img/cards/8h.jpg"
+  },
+  {
+    "revision": "1fccf774f0d8fb96a777a69c40d0d66c",
+    "url": "/img/cards/8s.jpg"
+  },
+  {
+    "revision": "0bffa88847970451d05f12559c2a48e1",
+    "url": "/img/cards/9c.jpg"
+  },
+  {
+    "revision": "f2666e5b48c519f46868122779c6bb2c",
+    "url": "/img/cards/9d.jpg"
+  },
+  {
+    "revision": "c5021565b19c2396d51664aa1acda678",
+    "url": "/img/cards/9h.jpg"
+  },
+  {
+    "revision": "221e370fea223c0295115b08eb5aee86",
+    "url": "/img/cards/9s.jpg"
+  },
+  {
+    "revision": "eb345eeb0d2a3eb039a4f94c25c68780",
+    "url": "/img/cards/ac.jpg"
+  },
+  {
+    "revision": "a95f5a2ddd6449b367d2e541a81a39f5",
+    "url": "/img/cards/ad.jpg"
+  },
+  {
+    "revision": "de87ce896c7d8a2032c31d975c96f180",
+    "url": "/img/cards/ah.jpg"
+  },
+  {
+    "revision": "261ad9d328bab90d78d739ab05329ee8",
+    "url": "/img/cards/as.jpg"
+  },
+  {
+    "revision": "80dd37823d80a0fc8746557c75083108",
+    "url": "/img/cards/back.jpg"
+  },
+  {
+    "revision": "e0a3ba9d70309638e98fe42300578bdc",
+    "url": "/img/cards/jc.jpg"
+  },
+  {
+    "revision": "fae90dce06ff0a2d7a74028633c60f54",
+    "url": "/img/cards/jd.jpg"
+  },
+  {
+    "revision": "4d8572cd3176dc056ef8fc2118f21fbe",
+    "url": "/img/cards/jh.jpg"
+  },
+  {
+    "revision": "9e8054fbb99bcb617cf69b901c38e66c",
+    "url": "/img/cards/js.jpg"
+  },
+  {
+    "revision": "134afe4fa927a6b63fd51b31c6779cad",
+    "url": "/img/cards/kc.jpg"
+  },
+  {
+    "revision": "954ccbc3f0e8416cba052716c75ad79b",
+    "url": "/img/cards/kd.jpg"
+  },
+  {
+    "revision": "9aecaab2eec8006276152352c847829c",
+    "url": "/img/cards/kh.jpg"
+  },
+  {
+    "revision": "51929e7194170dd475dd8f20644d4ee2",
+    "url": "/img/cards/ks.jpg"
+  },
+  {
+    "revision": "a6677599f552c18e762b21444d25ffa9",
+    "url": "/img/cards/qc.jpg"
+  },
+  {
+    "revision": "ff9f962e2c665eab5b4ce8e0428ba300",
+    "url": "/img/cards/qd.jpg"
+  },
+  {
+    "revision": "0ef0639fc5f0725389f10972eff6d3cb",
+    "url": "/img/cards/qh.jpg"
+  },
+  {
+    "revision": "aa95fd7fd712c9e403a36636fec8a3d7",
+    "url": "/img/cards/qs.jpg"
+  },
+  {
+    "revision": "b3da169cf3e8232a91b81fbf460430f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90277b8b5a7bba33cbfb",
+    "url": "/js/app.f6ef9c3a.js"
+  },
+  {
+    "revision": "a740de5f7c6d17276edf",
+    "url": "/js/chunk-vendors.abc77a09.js"
+  },
+  {
+    "revision": "cd4ba09cc5654911345a9dcb2dc43c39",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
